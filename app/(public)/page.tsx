846ec9a48c43ddd19d59a09ab320527024a5ed43
CodeWith-artist <mohit9599411352@gmail.com>
2026-05-12 09:26:45 -0700

    form

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/home/HeroSection";
+import QueryForm from "@/components/home/QueryForm";
 
 export default async function Home() {
   return (
    <>
       <HeroSection />
+      <QueryForm />
      
    </>
   );
