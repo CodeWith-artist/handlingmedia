9df0101ef062c799ad6a72109a092034add88276
CodeWith-artist <mohit9599411352@gmail.com>
2026-05-29 16:20:17 +0530

    increased font size of contact page heading

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -37,7 +37,7 @@ export default function ContactPage() {
           </div>
 
           <h1
-            className="font-black text-white leading-[0.88] mb-6"
+            className="font-black text-7xl text-white leading-[0.88] mb-6"
             
           >
             Let's build<br />
