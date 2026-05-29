dde01a15427ae526f2d00396c117eea7858003f4
CodeWith-artist <mohit9599411352@gmail.com>
2026-05-29 11:27:36 +0530

    changed the code again

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,32 +1,24 @@
-// import { Sidebar } from "./components/sidebar/Sidebar";
-// import "@/app/(public)/globals.css";
-// import { getSession } from "@/lib/auth/session";
+import { Sidebar } from "./components/sidebar/Sidebar";
+import "@/app/(public)/globals.css";
+import { getSession } from "@/lib/auth/session";
 
-// export default async function DashboardLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   const session = await getSession();
-
-//   return (
-//     <html lang="en">
-//       <body>
-//         <div className="flex h-screen bg-[#080c11] overflow-hidden text-white">
-//           <Sidebar session={session} />
-//           <main className="flex-1 overflow-auto">
-//               {children}
-//           </main>
-//         </div>
-//       </body>
-//     </html>
-//   );
-// }
-
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  const session = await getSession();
+
+  return (
+    <html lang="en">
+      <body>
+        <div className="flex h-screen bg-[#080c11] overflow-hidden text-white">
+          <Sidebar session={session} />
+          <main className="flex-1 overflow-auto">
+              {children}
+          </main>
+        </div>
+      </body>
+    </html>
+  );
 }
