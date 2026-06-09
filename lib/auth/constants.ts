5435df553ecf9ecd0f9d48214137371a1bbc3368
CodeWith-artist <mohit9599411352@gmail.com>
2026-06-09 16:43:15 +0530

    changed server time

--- lib/auth/constants.ts
+++ lib/auth/constants.ts
@@ -1,4 +1,4 @@
-export const ACCESS_TOKEN_EXPIRY_SECONDS = 15 * 60; // 15 minutes
+export const ACCESS_TOKEN_EXPIRY_SECONDS = 7 * 60 * 60; // 7 hours
 export const REFRESH_TOKEN_EXPIRY_SECONDS = 30 * 24 * 60 * 60; // 30 days
 
 export const ACCESS_COOKIE = "access_token";
